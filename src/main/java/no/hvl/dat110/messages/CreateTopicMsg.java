95732f9c502ccad3c7e52c2d216614d758acc409
599007 <599007@stud.hvl.no>
2023-02-16 13:43:41 +0100

    Done with task A

--- src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -3,12 +3,38 @@ package no.hvl.dat110.messages;
 public class CreateTopicMsg extends Message {
 	
 	// message sent from client to create topic on the broker
+    private String user;
+    private String topic;
 
     public CreateTopicMsg(String user, String topic) {
-
+        super(MessageType.CREATETOPIC, user);
+    this.user=user;
+    this.topic=topic;
     }
 
-	// TODO: 
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String getUser(){
+        return user;
+    }
+    public String getTopic(){
+        return topic;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateTopicMsg{" +
+                "user='" + user + '\'' +
+                ", topic='" + topic + '\'' +
+                '}';
+    }
+    // TODO:
+
 	// Implement object variables - a topic is required
 	
 	// Complete the constructor, get/set-methods, and toString method
--- src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -3,12 +3,39 @@ package no.hvl.dat110.messages;
 public class DeleteTopicMsg extends Message {
 
 	// message sent from client to create topic on the broker
-
+    String user;
+    String topic;
     public DeleteTopicMsg(String user, String topic) {
-
+        super(MessageType.DELETETOPIC, user);
+        this.user = user;
+        this.topic = topic;
     }
 
-	// TODO:
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Override
+    public String toString() {
+        return "DeleteTopicMsg{" +
+                "user='" + user + '\'' +
+                ", topic='" + topic + '\'' +
+                '}';
+    }
+    // TODO:
 	// Implement object variables - a topic is required
 
 	// Complete the constructor, get/set-methods, and toString method
